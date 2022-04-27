f149f6f8e5d599d2670aa211f8ef6231820c74f8
Trevor Reeves <trevor.reeves@gmail.com>
2022-04-27 20:38:40 +0100

    initial content

---
+++ init.sh
@@ -0,0 +1,2 @@
+docker pull wiremock/wiremock
+wget https://repo1.maven.org/maven2/com/opentable/wiremock-body-transformer/1.1.3/wiremock-body-transformer-1.1.3.jar   -O extensions/wiremock-body-transformer-1.1.3.jar
